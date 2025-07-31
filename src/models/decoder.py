c9777156eb4563a42b1a9e87e891ea3b47e579a7
Biobay <126084823+Biobay@users.noreply.github.com>
2025-07-31 12:39:04 +0200

    Update decoder.py

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -5,41 +5,73 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .attention import CrossAttentionBlock
 
-# --- ARCHITETTURA U-NET ROBUSTA (ISPIRATA A PIX2PIXHD) ---
+# --- Architettura U-Net Robusta ---
+
+class UNetDownBlock(nn.Module):
+    def __init__(self, in_c, out_c, norm=True):
+        super().__init__()
+        layers = [nn.Conv2d(in_c, out_c, 4, 2, 1, bias=not norm)]
+        if norm:
+            layers.append(nn.BatchNorm2d(out_c))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.model(x)
+
+class UNetUpBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(in_c, out_c, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        return self.model(x)
 
 class UNetDecoder(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
-        ngf = 64 # Numero di feature base
+        ngf = 64
 
-        # --- BLOCCHI DI DISCESA (ENCODER) ---
-        self.down1 = nn.Conv2d(cfg.OUTPUT_CHANNELS, ngf, kernel_size=4, stride=2, padding=1)
-        self.down2 = nn.Sequential(nn.LeakyReLU(0.2), nn.Conv2d(ngf, ngf * 2, 4, 2, 1), nn.BatchNorm2d(ngf * 2))
-        self.down3 = nn.Sequential(nn.LeakyReLU(0.2), nn.Conv2d(ngf * 2, ngf * 4, 4, 2, 1), nn.BatchNorm2d(ngf * 4))
-        self.down4 = nn.Sequential(nn.LeakyReLU(0.2), nn.Conv2d(ngf * 4, ngf * 8, 4, 2, 1), nn.BatchNorm2d(ngf * 8))
-        self.down5 = nn.Sequential(nn.LeakyReLU(0.2), nn.Conv2d(ngf * 8, ngf * 8, 4, 2, 1), nn.BatchNorm2d(ngf * 8))
+        # --- Percorso di Discesa (Encoder) ---
+        self.down1 = UNetDownBlock(cfg.OUTPUT_CHANNELS, ngf, norm=False)
+        self.down2 = UNetDownBlock(ngf, ngf * 2)
+        self.down3 = UNetDownBlock(ngf * 2, ngf * 4)
+        self.down4 = UNetDownBlock(ngf * 4, ngf * 8)
+        self.down5 = UNetDownBlock(ngf * 8, ngf * 8)
         
-        # --- BOTTLENECK ---
-        self.bottleneck = nn.Sequential(nn.LeakyReLU(0.2), nn.Conv2d(ngf * 8, ngf * 8, 4, 2, 1), nn.ReLU())
+        # --- Bottleneck ---
+        self.bottleneck = UNetDownBlock(ngf * 8, ngf * 8)
         
-        # --- BLOCCHI DI RISALITA (DECODER) ---
-        self.up1 = nn.Sequential(nn.ConvTranspose2d(ngf * 8, ngf * 8, 4, 2, 1), nn.BatchNorm2d(ngf * 8))
-        self.up2 = nn.Sequential(nn.ReLU(), nn.ConvTranspose2d(ngf * 8 * 2, ngf * 8, 4, 2, 1), nn.BatchNorm2d(ngf * 8))
-        self.up3 = nn.Sequential(nn.ReLU(), nn.ConvTranspose2d(ngf * 8 * 2, ngf * 4, 4, 2, 1), nn.BatchNorm2d(ngf * 4))
-        self.up4 = nn.Sequential(nn.ReLU(), nn.ConvTranspose2d(ngf * 4 * 2, ngf * 2, 4, 2, 1), nn.BatchNorm2d(ngf * 2))
-        self.up5 = nn.Sequential(nn.ReLU(), nn.ConvTranspose2d(ngf * 2 * 2, ngf, 4, 2, 1), nn.BatchNorm2d(ngf))
+        # --- Percorso di Risalita (Decoder) ---
+        # Nota: in_channels ora tiene conto della concatenazione
+        self.up1 = UNetUpBlock(ngf * 8, ngf * 8)
+        self.up2 = UNetUpBlock(ngf * 8 * 2, ngf * 4)
+        self.up3 = UNetUpBlock(ngf * 4 * 2, ngf * 2)
+        self.up4 = UNetUpBlock(ngf * 2 * 2, ngf)
+        self.up5 = UNetUpBlock(ngf * 2, ngf) # Aggiunto per simmetria
         
-        # --- LAYER FINALE ---
-        self.final_up = nn.Sequential(nn.ReLU(), nn.ConvTranspose2d(ngf * 2, cfg.OUTPUT_CHANNELS, 4, 2, 1), nn.Tanh())
-        
-        # --- BLOCCHI DI ATTENZIONE ---
+        # --- Blocchi di Attenzione ---
         self.attn1 = CrossAttentionBlock(query_dim=ngf * 8, context_dim=cfg.CONTEXT_DIM, num_heads=cfg.NUM_HEADS)
         self.attn2 = CrossAttentionBlock(query_dim=ngf * 4, context_dim=cfg.CONTEXT_DIM, num_heads=cfg.NUM_HEADS)
 
+        # --- Layer Finale ---
+        self.final_up = nn.ConvTranspose2d(ngf * 2, cfg.OUTPUT_CHANNELS, 4, 2, 1)
+        self.final_act = nn.Tanh()
+
     def forward(self, text_features):
         batch_size = text_features.size(0)
-        x = torch.randn(batch_size, self.cfg.OUTPUT_CHANNELS, self.cfg.MODEL_INTERNAL_SIZE, self.cfg.MODEL_INTERNAL_SIZE, device=text_features.device)
+        x = torch.randn(
+            batch_size, 
+            self.cfg.OUTPUT_CHANNELS, 
+            self.cfg.MODEL_INTERNAL_SIZE, 
+            self.cfg.MODEL_INTERNAL_SIZE, 
+            device=text_features.device
+        )
         
         def apply_attention(x, attn_block, context):
             B, C, H, W = x.shape
@@ -47,34 +79,36 @@ class UNetDecoder(nn.Module):
             attn_feat = attn_block(img_feat, context)
             return attn_feat.permute(0, 2, 1).view(B, C, H, W)
 
-        # --- PERCORSO DI DISCESA ---
-        d1 = self.down1(x)    # 256 -> 128
-        d2 = self.down2(d1)   # 128 -> 64
-        d3 = self.down3(d2)   # 64 -> 32
-        d4 = self.down4(d3)   # 32 -> 16
-        d5 = self.down5(d4)   # 16 -> 8
+        # --- Percorso di Discesa ---
+        d1 = self.down1(x)    # Out: 128x128, ngf
+        d2 = self.down2(d1)   # Out: 64x64, ngf*2
+        d3 = self.down3(d2)   # Out: 32x32, ngf*4
+        d4 = self.down4(d3)   # Out: 16x16, ngf*8
+        d5 = self.down5(d4)   # Out: 8x8, ngf*8
         
-        b = self.bottleneck(d5) # 8 -> 4
+        b = self.bottleneck(d5) # Out: 4x4, ngf*8
         
-        # --- PERCORSO DI RISALITA ---
-        u1 = self.up1(b)                 # 4 -> 8
-        u1 = torch.cat([u1, d5], dim=1)  # Concatena con d5 (8x8)
-        u1 = apply_attention(u1, self.attn1, text_features) # Applica attention qui
+        # --- Percorso di Risalita con Ordine Corretto ---
         
-        u2 = self.up2(u1)                # 8 -> 16
-        u2 = torch.cat([u2, d4], dim=1)  # Concatena con d4 (16x16)
+        u1_out = self.up1(b)                 # 4x4 -> 8x8
+        u1_attn = apply_attention(u1_out, self.attn1, text_features)
+        u1_cat = torch.cat([u1_attn, d5], dim=1)
         
-        u3 = self.up3(u2)                # 16 -> 32
-        u3 = torch.cat([u3, d3], dim=1)  # Concatena con d3 (32x32)
-        u3 = apply_attention(u3, self.attn2, text_features) # Applica attention qui
+        u2_out = self.up2(u1_cat)            # 8x8 -> 16x16
+        u2_attn = apply_attention(u2_out, self.attn2, text_features)
+        u2_cat = torch.cat([u2_attn, d4], dim=1)
         
-        u4 = self.up4(u3)                # 32 -> 64
-        u4 = torch.cat([u4, d2], dim=1)  # Concatena con d2 (64x64)
+        u3_out = self.up3(u2_cat)            # 16x16 -> 32x32
+        u3_cat = torch.cat([u3_out, d3], dim=1)
         
-        u5 = self.up5(u4)                # 64 -> 128
-        u5 = torch.cat([u5, d1], dim=1)  # Concatena con d1 (128x128)
+        u4_out = self.up4(u3_cat)            # 32x32 -> 64x64
+        u4_cat = torch.cat([u4_out, d2], dim=1)
         
-        out = self.final_up(u5)          # 128 -> 256
+        u5_out = self.up5(u4_cat)            # 64x64 -> 128x128
+        u5_cat = torch.cat([u5_out, d1], dim=1)
+
+        # --- Layer Finale ---
+        out = self.final_up(u5_cat) # 128x128 -> 256x256
         
         final_image = F.interpolate(out, size=self.cfg.IMAGE_OUTPUT_SIZE, mode='bilinear', align_corners=False)
-        return final_image, None
+        return self.final_act(final_image), None 
